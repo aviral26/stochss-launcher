d5ea64eef9fec1e6cae8bc68a2da72ff4b7267da
Aviral Takkar <aviraltakkar@gmail.com>
2016-02-24 22:28:23 -0800

    fix app bundles

--- OSX/uninstall1.7.sh
+++ OSX/uninstall1.7.sh
@@ -4,16 +4,16 @@ docker-machine version || (echo "Looks like docker-machine is not installed; no 
 docker-machine start stochssdocker >> .uninstallLog 2>&1
 docker-machine env stochssdocker >> .uninstallLog || (echo "cannot set environment" && exit -1)
 eval "$(docker-machine env stochssdocker)"
-(docker stop stochsscontainer1_7 >> .uninstallLog 2>&1 && docker rm stochsscontainer1_7) || (echo "Could not remove StochSS container version 1.7. Exiting.." && exit -1)
+(docker stop stochsscontainer1_7 >> .uninstallLog 2>&1 && docker rm stochsscontainer1_7) || echo "StochSS container does not exist."
 num_containers=`docker ps -aq | wc -l`
 if [[ `echo $num_containers` == 0 ]]
 then 
-	echo "Safe to delete VM."
+	echo "Safe to delete VM. (I have commented out the actual command to delete the VM just to make debugging easier)"
 	#docker-machine rm stochssdocker || (echo "Could not remove VM. Exiting.." && exit 0)
 else 
  	echo "Not deleting VM as it has other containers."
 fi
-echo "Successfully uninstalled."
+echo "Done"
 read -p "Press any key to exit." key
 exit 0
 
